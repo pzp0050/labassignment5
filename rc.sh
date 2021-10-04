e8eb34c1d0fbebbf9dac5ffba32301720331c324
Prathibha Prasanna Chandran <pzp0050@auburn.edu>
2021-10-04 17:38:14 -0500

    I added an if condition to give an error message if not fasta input is given, and removed the command to echo the reverse complement sequence.

--- rc.sh
+++ rc.sh
@@ -1,4 +1,12 @@
-#! /bin/sh
+#!/bin/sh
+
+# help comment
+
+if [ $# -lt 1 ] || [ $# -gt 1 ];
+then
+echo "Please provide a fasta file in the input"
+exit
+fi
 
 #initialize variable
 reverse=""
@@ -8,16 +16,16 @@ name=`grep ">" $1`
 sequence=`grep -v ">" $1`
 
 echo "Name: $name"
-
+echo "sequence: $sequence"
 #calculate length of sequence
 len=${#sequence}
 
 echo "Length: $len"
 
 #loop through sequence in reverse
-for (( i=$len; i>=0; i-- ))
+for ((i=$len; i>=0; i--))
 do
-    reverse="$reverse${sequence:$i:1}"
+    reverse="$reverse${$sequence:$i:1}"
 done
 
 echo "$sequence
@@ -26,9 +34,6 @@ $reverse
 
 #complement sequence
 rc=`echo $reverse | tr 'atcg' 'tagc'`
-
-echo "$rc
-"
 
 echo $name >$1.rc.txt
 echo $rc >>$1.rc.txt
